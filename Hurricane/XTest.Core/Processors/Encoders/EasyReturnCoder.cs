c450fbd6d2691c36135ca0f1887ea16b6b4a179d
Yura <onyfrak1999@ukr.net>
2019-01-05 16:43:20 +0200

    EasyReturnCoder

--- Hurricane/XTest.Core/Processors/Encoders/EasyReturnCoder.cs
+++ Hurricane/XTest.Core/Processors/Encoders/EasyReturnCoder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Text;
 using Hurricane.XTest.Core.Abstract.Entities;
 using Hurricane.XTest.Core.Const.Enums;
+using Hurricane.XTest.Core.Entities;
 
 namespace Hurricane.XTest.Core.Processors.Encoders
 {
@@ -26,13 +28,88 @@ namespace Hurricane.XTest.Core.Processors.Encoders
             CodeType = codeType;
         }
 
+
         private IQuestionEntity Encoder()
         {
-            throw new Exception();
+            IQuestionEntity questionEntity = new QuestionEntity();
+            questionEntity.QuestionType = QuestionType.EasyReturn;
+            questionEntity.CodeType = CodeType;
+
+            questionEntity.Description = "Закодируйте сообщение";
+
+            StringBuilder answer = new StringBuilder();
+            StringBuilder questionValue = new StringBuilder();
+
+            int countNumber = _random.Next(3, 9);
+
+            for (int i = 0; i < countNumber; i++)
+            {
+                if (i == 0)
+                {
+                    questionValue.Append(_random.Next(1, 9));
+                }
+                else
+                {
+                    questionValue.Append(_random.Next(0, 9));
+                }
+            }
+
+            answer.Append(questionValue); answer.Append(questionValue);
+
+            questionEntity.Question = new BaseValue()
+            {
+                Value = questionValue.ToString()
+            };
+
+            questionEntity.Answer = new BaseValue()
+            {
+                Value = answer.ToString()
+            };
+
+            return questionEntity;
         }
+
         private IQuestionEntity Decoder()
         {
-            throw new Exception();
+            IQuestionEntity questionEntity = new QuestionEntity();
+            questionEntity.QuestionType = QuestionType.EasyReturn;
+            questionEntity.CodeType = CodeType;
+
+            questionEntity.Description = "Раскодируйте сообщение";
+
+            StringBuilder answer = new StringBuilder();
+
+            StringBuilder questionValue = new StringBuilder();
+
+            int countNumber = _random.Next(3, 9);
+
+            for (int i = 0; i < countNumber; i++)
+            {
+                if (i == 0 )
+                {
+                    answer.Append(_random.Next(1, 9));
+                }
+                else
+                {
+                    answer.Append(_random.Next(0, 9));
+                }
+               
+            }
+
+            questionValue.Append(answer); questionValue.Append(answer);
+
+            questionEntity.Answer = new BaseValue()
+            {
+                Value = answer.ToString()
+            };
+
+            questionEntity.Question = new BaseValue()
+            {
+                Value = questionValue.ToString()
+            };
+
+            return questionEntity;
         }
+
     }
 }
